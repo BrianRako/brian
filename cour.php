0d60ad98f98f53e9999c4df18fcb85fc1efd40a6
Crunch01 <48915287+Crunch01@users.noreply.github.com>
2020-12-17 12:59:30 +0100

    add class User

--- cour.php
+++ cour.php
@@ -4,16 +4,8 @@
 
 require_once 'user.php';
 
-$moto = new Moto();
-$moto->brand = "Honda";
-$moto->color = "verte";
-$moto->maxSpeed = 238;
+$kawa = new Moto('Kawasaki', 'rouge', '217');
+$bm = new Moto('BMW', 'bleue', '210');
 
-echo $moto->getDescription();
-
-$moto2 = new Moto();
-$moto2->brand = 'Kawasaki';
-$moto2->color = 'noir';
-$moto2->maxSpeed = 238;
-
-echo $moto2->getDescription();
+$race = new Race($kawa, $bm);
+echo $race->StartRace()->getDescription();
--- user.php
+++ user.php
@@ -1,18 +1,54 @@
 <?php
 
 // Fichier User.php
-define('br', '<br>');
+define('BR', '<br>');
+
 
 class Moto
 {
+    public string $brand;
+    public string $color;
+    public float $maxSpeed;
 
-    public string  $brand;
-    public string  $color;
-    public float   $maxSpeed;
 
-    public function getDescription():string
+    public function __construct(string $brand, string $color, float $maxSpeed)
     {
+        $this->brand    = $brand;
+        $this->color    = $color;
+        $this->maxSpeed = $maxSpeed;
 
-        return $this->brand. ' ' .$this->color. ' ayant une vitesse de ' .$this->maxSpeed. ' km/h.'.br;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->brand.' '.$this->color.' ayant une vitesse maximale de '.$this->maxSpeed.'km/h'.BR;
+    }
+
+    /**  public function __destruct()
+    {
+        echo $this->brand . ' ' . $this->color . ' rentre au garage' . BR;
+    }
+     */
+
+}
+
+class Race
+{
+    public moto $moto1;
+    public moto $moto2;
+
+    public function __construct(moto $moto1, moto $moto2)
+    {
+        $this->moto1 = $moto1;
+        $this->moto2 = $moto2;
+    }
+
+    public function StartRace()
+    {
+        if ($this->moto1->maxSpeed > $this->moto2->maxSpeed)
+        {
+            return $this->moto1;
+        }
+        return $this->moto2;
     }
 }
